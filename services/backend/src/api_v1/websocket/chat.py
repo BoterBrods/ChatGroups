8ae02dc4e559168203b1775ed712f49465eb270d
Tim Lushikov <tim-000@github.com>
2025-05-31 16:52:30 +0300

    Added basic chat

--- services/backend/src/api_v1/websocket/chat.py
+++ services/backend/src/api_v1/websocket/chat.py
@@ -1,6 +1,20 @@
-def main():
-    pass
+import json
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+
+from ConnectionManager import manager
 
 
-if __name__=="__main__":
-    main()
+app = FastAPI()
+
+
+
+@app.websocket("/ws}")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_to_all(data)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.send_to_all("Пользователь покинул чат")
