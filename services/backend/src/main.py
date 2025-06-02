a851e7b2d3e789bb93d3556b014c6b3ecaeb6dd2
VladislavVladimirovv <vladimirovvladislav198@gmail.com>
2025-06-02 21:17:22 +0300

    added: frontend auth

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -7,6 +7,7 @@ from core.models.db_helper import db_helper
 from core.models.base import Base
 from api_v1 import router
 
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -15,11 +16,21 @@ async def lifespan(app: FastAPI):
     yield
     await db_helper.dispose()
 
-
 app = FastAPI(lifespan=lifespan)
-app.include_router(router, prefix="/api")
 
-app.include_router(router_chat_groups, prefix='/api')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5174"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router, prefix="/api")
+app.include_router(router_chat_groups, prefix="/api")
+
+
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
